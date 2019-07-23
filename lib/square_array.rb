b3a2f3de7f5209a7208b228850daea788ec7a00b
Rohan Phillips <rohan@solutions-insurance.com>
2019-07-23 21:06:01 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -2,7 +2,10 @@ def square_array(array)
   # your code here
   newnumbers = []
   x = 0 
+  num = 0 
   while x < array.length do
-      
+    num = array[x]
+    newnumbers.push(num * num)
   end
+  return newnumbers
 end
